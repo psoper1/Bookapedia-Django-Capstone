faa730d6c3d31fb439584ca38c0602a26b1a91af
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-13 16:12:13 +0000

    finished setup

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
