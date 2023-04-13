da73d2e93ef8b8023e2ee97def200f385c124e7b
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-13 14:39:38 +0000

    setup backend

---
+++ bookapedia/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    birthday = models.DateField(null=True)
+
+    def __str__(self):
+        return self.username
