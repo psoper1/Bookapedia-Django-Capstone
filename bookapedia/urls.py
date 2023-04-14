e40f5e1253a73bb42efbf5ce5459da4767464b98
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-14 22:42:13 +0000

    added urls I may have been missing

---
+++ bookapedia/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'messages', MessageViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
+]
