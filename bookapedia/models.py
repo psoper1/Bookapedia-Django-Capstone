c128a27eaf8eacf60a914ad404c94f1ece7faa80
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-17 23:07:57 +0000

    started models for books and the bookshelf but did NOT migrate yet

--- bookapedia/models.py
+++ bookapedia/models.py
@@ -19,3 +19,33 @@ class CustomUser(AbstractUser):
         db_table = "users"
 
 print("test")
+
+# Started models for the saved book and the bookshelf
+# The table SHOULD be a many-to-many table
+# It is not at the moment so I can visualize it
+# The table should have the ID of the user which is always the same
+# And should reference the ID in the API of the book
+# I think getting the ID from the API ID of the book
+# would be more consistant because each book has a special ID
+# This is because if the same book is saved between
+# different users, I don't want the same book listed twice
+# with different ID'set
+# This way the table will grab the books API ID instead
+
+class Book(models.Model):
+    title = models.CharField(max_length=500, null=False)
+    author = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=2000, null=True)
+    genre = models.CharField(max_length=300, null=True)
+    date_published = models.DateField(null=True)
+    marked_read = models.BooleanField(null=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+class Bookshelf(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
